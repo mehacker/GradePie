c8e63ff3e6be22b1f4a987342b7bb2ccabc40722
Nathan Thai Nguyen <mrrobot@nathans-macbook.local>
2015-10-06 14:34:26 -0400

    test commit

--- GradePie/ViewController.swift
+++ GradePie/ViewController.swift
@@ -8,17 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView = UITableView()
+    
+    var items: [String] = [ "Chemistry" , "Mathematics", "Computer Science"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.frame = CGRectMake(50, 50, 120, 300)
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.addSubview(tableView)
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        
+        
+   
+        
+        let rightPickerImageView = UIImageView(frame: CGRectMake(10, 10, 120, 60))
+        let rightPicker : UIImage = UIImage(named: "rightarrow")!
+        rightPickerImageView.image = rightPicker
+        
+        
+        cell.backgroundView = UIView()
+        cell.backgroundView?.addSubview(rightPickerImageView)
+        
+        //cell.imageView!.image = image
+        
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
+        
+        
+                // Do any additional setup after loading the view, typically from a nib.
+        
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
+
+  
+    
 
 
 }
